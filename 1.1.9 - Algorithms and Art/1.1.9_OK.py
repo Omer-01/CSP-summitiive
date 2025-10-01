cb98ae0f20fe2dd1ad2a5a45b2a6510d1917e04b
Omer-01 <300668@shakopeeschools.org>
2025-10-01 09:04:22 -0500

    Update 1.1.9_OK.py
    added garage door, front door, house windows and driveway.

--- 1.1.9 - Algorithms and Art/1.1.9_OK.py
+++ 1.1.9 - Algorithms and Art/1.1.9_OK.py
@@ -28,19 +28,53 @@ painter.goto(160,140)
 painter.pendown()
 
 painter.circle(140,180,3)
+painter.penup()
 # draw house windows
+painter.setheading(45)
 
+painter.goto(-83,10)
+painter.pendown()
+painter.circle(20,360,4)
+painter.penup()
 
-# draw house door
+painter.setheading(45)
 
+painter.goto(-83,50)
+painter.pendown()
+painter.circle(20,360,4)
+painter.penup()
 
-# draw house garage
+# draw house front door
+painter.setheading(45)
 
+painter.goto(1,0)
+painter.pendown()
+painter.circle(50,360,4)
+painter.penup()
+
+# draw house garage door
+painter.setheading(45)
+
+painter.goto(145,0)
+painter.pendown()
+painter.circle(80,360,4)
+painter.penup()
 
 
 # draw house driveway 
+painter.setheading(45)
 
+painter.goto(18,-85)
+painter.pendown()
+painter.circle(60,360,4)
+painter.penup()
 
+painter.setheading(45)
+
+painter.goto(160,-140)
+painter.pendown()
+painter.circle(100,360,4)
+painter.penup()
 
 # ---------------------
 wn = trtl.Screen()
