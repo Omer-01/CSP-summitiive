d94d6a1d413be428d2c7b4d6128d887bb4347b6d
Omer-01 <300668@shakopeeschools.org>
2025-10-28 11:17:51 -0500

    Create 1.2.5_OK.py
    Added milestones and added important starting code

---
+++ 1.2.5 - Shall We Play a Game/1.2.5_OK.py
@@ -0,0 +1,36 @@
+import turtle as trtl
+import random as rand
+
+myPen = trtl.Screen()
+# --- Game Setup ---
+
+
+
+
+# --- Boss Code ---
+
+
+
+
+
+# --- 1st Option ---
+
+
+
+
+
+# --- 2nd Option ---
+
+
+
+
+
+# --- 3rd option ---
+
+
+
+
+
+# --- --- - - - - --
+wn = trtl.Screen()
+wn.mainloop()
